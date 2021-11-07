79d58d751b1fbb133b47a365fcf118f560f1d323
Matt Wilkinson <mattwilki17@gmail.com>
2021-11-07 13:17:41 -0500

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:ferry/ferry.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'pages/login_page.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 void setup() async {
   final client = await initClient();
